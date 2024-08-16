d3b90a5b00ff973d8fccb85be256e3fb43718359
abcdmku <63693423+abcdmku@users.noreply.github.com>
2024-08-16 13:43:56 -0500

    basehref from subdir

--- apps/clue/vite.config.ts
+++ apps/clue/vite.config.ts
@@ -6,7 +6,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 export default defineConfig({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/clue',
-
+  base: '/clue/',
   server: {
     port: 4200,
     host: 'localhost',
