509bc791fff3e4a67b30c04ac416230abcfeb863
abcdmku <63693423+abcdmku@users.noreply.github.com>
2024-08-17 23:24:25 -0500

    Update rotatingAngle.tsx

--- libs/comp-lib/src/lib/rotatingAngle.tsx
+++ libs/comp-lib/src/lib/rotatingAngle.tsx
@@ -2,6 +2,7 @@ export const RotatingArrow = ({ angle = 0 }) => {
   const arrowStyle: React.CSSProperties = {
     transform: `rotate(${angle || 0}deg)`,
     transition: 'transform 0.3s ease-in-out',
+    fontSize: '10rem',
   };
 
   return <div style={arrowStyle}>↑</div>;
